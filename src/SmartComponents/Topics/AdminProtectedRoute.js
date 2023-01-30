fc9e21b4e7007499d552f00577f9ca9adc789f11
Aleksandr Voznesenskii <avoznese@redhat.com>
2023-01-30 13:39:15 +0100

    chore(codecov):rebuilding the pr

--- src/SmartComponents/Topics/AdminProtectedRoute.js
+++ src/SmartComponents/Topics/AdminProtectedRoute.js
@@ -11,7 +11,6 @@ const AdminProtectedRoute = ({ component: Component, ...props }) => {
     isInternal: false,
     loaded: false,
   });
-
   useEffect(() => {
     insights.chrome.auth
       .getUser()
